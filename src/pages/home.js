d250b9dacc1ca88942a095577111cb491cfd1b90
k-natori1025 <k-natori1025@gmail.com>
2024-02-24 15:09:56 +0900

    TMDB APIから映画情報を取得

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,30 @@
 import AppLayout from '@/components/Layouts/AppLayout'
+import axios from 'axios'
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 const Dashboard = () => {
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await axios.get('api/getPopularMovies')
+                console.log(response)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        fetchMovies()
+    }, [])
     return (
         <AppLayout
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Dashboard
+                    Home
                 </h2>
             }>
             <Head>
-                <title>Laravel - Dashboard</title>
+                <title>Laravel - Home</title>
             </Head>
 
             <div className="py-12">
